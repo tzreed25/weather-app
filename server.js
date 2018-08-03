921a9fcabe57c1ecfa8b03a7b45101feaa99ee9d
tylerzreed <tylerzreed@gmail.com>
2018-08-02 23:05:06 -0400

    Retrieve city data using api call and pass data to ejs template

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = '3536fdb4c1692515cb64dc1319ae6dd2';
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    res.render('index');
+});
+
+app.post('/', function(req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    request(url, function(err, response, body) {
+        if(err) {
+            res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+            let weather = JSON.parse(body);
+            if(weather.main == undefined) {
+                res.render('index', {weather: null, error: 'Error, please try again'});
+            } else {
+                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                res.render('index', {weather: weatherText, error: null});
+            }
+        }
+    });
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Weather app listening on port 3000');
+});
